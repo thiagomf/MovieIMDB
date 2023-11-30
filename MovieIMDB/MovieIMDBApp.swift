cc47c324936b20e3149b9f76d65d3fe7c85016d7
Thiago m Faria <thiagomachadofaria@hotmail.com>
2023-11-30 18:14:04 -0300

    Initial Commit

---
+++ MovieIMDB/MovieIMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieIMDBApp.swift
+//  MovieIMDB
+//
+//  Created by Thiago M Faria on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieIMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieIMDBUITests/MovieIMDBUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieIMDBUITestsLaunchTests.swift
+//  MovieIMDBUITests
+//
+//  Created by Thiago M Faria on 30/11/23.
+//
+
+import XCTest
+
+final class MovieIMDBUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
