4035639ef9c7de56db5a6efd0447e831b5c33fd1
Thiago m Faria <thiagomachadofaria@hotmail.com>
2023-12-01 12:29:50 -0300

    endpoint configure TMDB

---
+++ MovieIMDB/Endpoint/MoviesEndpoint.swift
@@ -0,0 +1,59 @@
+//
+//  MoviesEndPoint.swift
+//  MovieIMDB
+//
+//  Created by Thiago M Faria on 01/12/23.
+//
+import NetworkLayer
+
+enum MoviesEndoint {
+    case topRated
+    case movieDetail(id: Int)
+}
+
+extension MoviesEndoint: EndPoint {
+    
+    var scheme: String {
+        return "https"
+    }
+    
+    
+    var host: String {
+        return "api.themoviedb.org"
+    }
+    
+    var path: String {
+        switch self {
+        case .topRated :
+            return "/3/movie/top_rated"
+        case .movieDetail(let id):
+            return "/3/movie/\(id)"
+        }
+    }
+    
+    var method: RequestMethod {
+        switch self {
+        case .topRated, .movieDetail:
+            return .get
+        }
+    }
+    
+    var header: [String : String]? {
+        let accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNjllZDNkMDEzYTliYTg0NzNlZWVmZDAwMTg4OWUxYSIsInN1YiI6IjVmMjIyNjMzY2FhY2EyMDAzNDY4MjcwNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FoUTRlT41OXn1uhnfTbOOfY3vOOXSMobSR2cY60ixp8"
+        switch self {
+        case .topRated, .movieDetail:
+            return [
+                "Authorization": "Bearer \(accessToken)",
+                "Content-Type": "application/json;charset=utf-8"
+            ]
+        }
+    }
+    
+    var body: [String : String]? {
+        switch self {
+        case .topRated, .movieDetail:
+            return nil
+        }
+    }
+    
+}
---
+++ MovieIMDB/Models/TopRated.swift
@@ -0,0 +1,21 @@
+//
+//  TopRated.swift
+//  MovieIMDB
+//
+//  Created by Thiago M Faria on 01/12/23.
+//
+import Foundation
+
+struct TopRated: Codable {
+    let page: Int
+    let totalPages: Int
+    let totalResults: Int
+    let results: [Movie]
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
