f9308829b070c43b980e5c8531fdcb1209b7919f
Thiago m Faria <thiagomachadofaria@hotmail.com>
2023-12-05 18:09:13 -0300

    create some navigations with full screen and sheets

---
+++ MovieIMDB/MovieView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieView.swift
+//  MovieIMDB
+//
+//  Created by Thiago M Faria on 05/12/23.
+//
+
+import SwiftUI
+
+struct MovieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieView()
+}
---
+++ MovieIMDB/WannaSeeView.swift
@@ -0,0 +1,18 @@
+//
+//  WannaSeeView.swift
+//  MovieIMDB
+//
+//  Created by Thiago M Faria on 05/12/23.
+//
+
+import SwiftUI
+
+struct WannaSeeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WannaSeeView()
+}
