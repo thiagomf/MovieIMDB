fe88eac0131d27483bd9793937853908d17fb140
Thiago m Faria <thiagomachadofaria@hotmail.com>
2023-12-05 14:02:00 -0300

    Movie List View

---
+++ MovieIMDB/MovieListView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListView.swift
+//  MovieIMDB
+//
+//  Created by Thiago M Faria on 05/12/23.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieListView()
+}
