9c686fde889f5c028ac1e64753cedb7ffa508688
Thiago m Faria <thiagomachadofaria@hotmail.com>
2023-12-05 12:06:00 -0300

    Movie Model implementations

--- MovieIMDB/Models/MovieModel.swift
+++ MovieIMDB/Models/MovieModel.swift
@@ -4,5 +4,39 @@
 //
 //  Created by Thiago M Faria on 05/12/23.
 //
+import UIKit
+import SwiftUI
+import NetworkLayer
 
-import Foundation
+@MainActor
+class MovieModel: ObservableObject {
+    
+    @Published var movies: [Movie] = []
+    @Published var movie : Movie? = nil
+    
+    let movieService: MoviesService
+    
+    init(movieService: MoviesService) {
+        self.movieService = movieService
+    }
+
+    func getAllMovies() async {
+        let result = await movieService.getTopRated()
+        switch result {
+        case .success(let response):
+            self.movies = response.results
+        case .failure(let error):
+            print("Erro to get all movies: \(error)")
+        }
+    }
+    
+    func getIDMovie(id: Int) async {
+        let result = await movieService.getMovieDetail(id: id)
+        switch result {
+        case .success(let response):
+            self.movie = response
+        case .failure(let error):
+            print("Erro to get \(id) Movie: \(error)")
+        }
+    }
+}
